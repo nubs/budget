fdb38f1b4e92fe73360faa63a4781625f6d7edb3
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2013-06-05 11:46:54 -0400

    Add support for income accounts via a -1 polarity.

--- models/budget.php
+++ models/budget.php
@@ -10,10 +10,10 @@ return function(MongoDB $db) {
         'find' => function(array $conditions = array()) use($collection) {
             return array_map(function($budget) {
                 $budget['total'] = array_sum(array_map(function($item) { return $item['amount']; }, $budget['items']));
-                $budget['overrun'] = $budget['total'] > 0;
+                $budget['overrun'] = $budget['polarity'] * $budget['total'] > 0;
 
                 return $budget;
-            }, iterator_to_array($collection->find($conditions)->sort(['title' => 1])));
+            }, iterator_to_array($collection->find($conditions)->sort(['polarity' => 1, 'title' => 1])));
         },
         'findOne' => $findOne,
         'addItem' => function($id, array $item) use($collection) {
